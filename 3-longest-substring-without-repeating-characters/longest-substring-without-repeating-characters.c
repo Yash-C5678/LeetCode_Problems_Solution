100cf3a62af362c390f3af32a82b51ff4b4c7517
Yash Choudhary <yashc2605@gmail.com>
2026-07-18 18:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (44.66%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
@@ -0,0 +1,23 @@
+int lengthOfLongestSubstring(char* s) {
+    int lastIndex[128];
+
+    for (int i = 0; i < 128; i++)
+        lastIndex[i] = -1;
+
+    int maxLength = 0;
+    int start = 0;
+
+    for (int end = 0; s[end] != '\0'; end++) {
+        if (lastIndex[(int)s[end]] >= start)
+            start = lastIndex[(int)s[end]] + 1;
+
+        lastIndex[(int)s[end]] = end;
+
+        int currentLength = end - start + 1;
+
+        if (currentLength > maxLength)
+            maxLength = currentLength;
+    }
+
+    return maxLength;
+}
